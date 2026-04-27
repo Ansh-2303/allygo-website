dc1ee3193e973fee95277755d8a2e1abe73cbe85
Ansh-2303 <anshpatel184@gmail.com>
2026-04-27 17:43:30 +0530

    Fix App.tsx merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-=======
->>>>>>> e1f9f2d8d12f0cf11c73d8571c178157bf0f5f1f
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Problem from './components/Problem';
@@ -12,21 +9,13 @@ import Pilot from './components/Pilot';
 import Internships from './components/Internships';
 import VisionTeam from './components/VisionTeam';
 import Footer from './components/Footer';
-<<<<<<< HEAD
-import Admin from './pages/Admin';
 
-// NEW (we'll create this next)
+import Admin from './pages/Admin';
 import Verify from './pages/Verify';
 
 function Home() {
   return (
     <>
-=======
-
-function App() {
-  return (
-    <div className="min-h-screen bg-white">
->>>>>>> e1f9f2d8d12f0cf11c73d8571c178157bf0f5f1f
       <Header />
       <Hero />
       <Problem />
@@ -36,7 +25,6 @@ function App() {
       <Internships />
       <VisionTeam />
       <Footer />
-<<<<<<< HEAD
     </>
   );
 }
@@ -54,10 +42,3 @@ function App() {
 }
 
 export default App;
-=======
-    </div>
-  );
-}
-
-export default App;
->>>>>>> e1f9f2d8d12f0cf11c73d8571c178157bf0f5f1f
